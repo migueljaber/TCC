cec084851767893a7dc140a6f90c347f19d1112d
Miguel-Jaber <128867836+Miguel-Jaber@users.noreply.github.com>
2024-04-03 00:14:55 -0300

    IDE FormSubmit no site principal

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-include "conexao.php";    
-    $nome=filter_input(INPUT_POST, "nome", FILTER_SANITIZE_STRING);
-    $mensagem=filter_input(INPUT_POST, "mensagem", FILTER_SANITIZE_STRING);
-    
-    $result_usuario="INSERT INTO feedback (nome, mensagem) VALUES ('$nome', '$mensagem')";
-    $SQL=mysqli_query($conn, $result_usuario);
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -212,8 +201,9 @@ include "conexao.php";
         <section class="formulario" id="formulario">
             <div class="interface">
                 <h2 class="titulo">FALE <span>CONOSCO</span></h2>
-                <form name="form_msg" id="form_msg" action="index.php" method="POST">
-                    <input type="text" name="nome" id="nome" placeholder="Nome completo" required>
+                <form name="form_msg" id="form_msg" action="https://formsubmit.co/advancedessoft@gmail.com" method="POST">
+                    <input type="text" name="nome" id="nome" placeholder="Nome" required>
+                    <input type="email" name="email" id="email" placeholder="E-mail" required>
                     <textarea name="mensagem" id="mensagem" placeholder="Mensagem" required></textarea>
                     <div class="advance-enviar"> <input type="submit" value="ENVIAR"></div>
                 </form>
