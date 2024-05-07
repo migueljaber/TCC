f66e5dcbf00bdc6c864282d24973ef33174d0264
Andrei Sousa <andreifernandezdevv@gmail.com>
2024-05-07 11:36:08 -0300

    inicialização da tela de cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,51 +1,24 @@
-/* estilo geral */
-*{
-
-    margin: 0;
-    padding: 0%;
-    box-sizing: border-box;
-    font-family: "Poppins", sans-serif;
-}
-
-body{
-    background-color: rgb(0, 0, 0);
-    height:100vh;
-	overflow:auto;
-
-}
-.interface{
-    max-width: 1280px;
-    margin: 0 auto;
-}
-.flex{
-    display: flex;
-}
-p.txt {
-    font-size: 20px;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-}
-
-
-.advance-contato button {
-    padding: 10px 40px;
-    font-size: 25px;
-    font-weight: 600;
-    background-color: #128f81;
-    border: 0;
-    border-radius: 50px;
-    cursor: pointer;
-    transition: .2s;
-}
-h2.titulo{
-       color: white;
-       font-size: 36px;
-       text-align: center;
-
-}
-h2.titulo span{
-    color: #128f81;
-}
- button:hover, form .advance-enviar input:hover{
-    box-shadow: 0px 0px 8px #1ec2af;
-    transform: scale(1.10);
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cadastro.css">
+    <title>Document</title>
+</head>
+<body>
+   
+<section class="formulario" id="formulario">
+            <div class="interface">
+                <h2 class="titulo">FALE <span>CONOSCO</span></h2>
+                <form name="form_msg" id="form_msg" action="https://formsubmit.co/advancedessoft@gmail.com" method="POST">
+                    <input type="text" name="nome" id="nome" placeholder="Nome" required>
+                    <input type="email" name="email" id="email" placeholder="E-mail" required>
+                    <input type="number" name="CPF" id="CPF" placeholder="CPF" required>
+                    <textarea name="mensagem" id="mensagem" placeholder="Mensagem" required></textarea>
+                    <div class="advance-enviar"> <input type="submit" value="ENVIAR"></div>
+                </form>
+            </div>
+        </section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -197,17 +197,7 @@
         </section>
     
      
-        <section class="formulario" id="formulario">
-            <div class="interface">
-                <h2 class="titulo">FALE <span>CONOSCO</span></h2>
-                <form name="form_msg" id="form_msg" action="https://formsubmit.co/advancedessoft@gmail.com" method="POST">
-                    <input type="text" name="nome" id="nome" placeholder="Nome" required>
-                    <input type="email" name="email" id="email" placeholder="E-mail" required>
-                    <textarea name="mensagem" id="mensagem" placeholder="Mensagem" required></textarea>
-                    <div class="advance-enviar"> <input type="submit" value="ENVIAR"></div>
-                </form>
-            </div>
-        </section>
+      
     </main>
 
     <footer>
